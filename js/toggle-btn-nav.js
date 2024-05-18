d3e9084e8e8ee1142bc27c6571b450a228d69999
Diego <zxiteft21@gmail.com>
2024-05-18 04:32:11 -0500

    First Commit

---
+++ js/toggle-btn-nav.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById("toggle-btn");
+const dropdownNav = document.getElementById("dropdown-nav");
+
+toggleButton.addEventListener("click",()=>{
+    dropdownNav.style.display = "flex"
+})
