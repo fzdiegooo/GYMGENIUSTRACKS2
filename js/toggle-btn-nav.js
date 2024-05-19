34aed26a1a78114e6406cf9433d1b1beb0f11c11
Diego <zxiteft21@gmail.com>
2024-05-19 01:30:42 -0500

    Pricing Layout Fixed

--- js/toggle-btn-nav.js
+++ js/toggle-btn-nav.js
@@ -1,6 +1,15 @@
 const toggleButton = document.getElementById("toggle-btn");
 const dropdownNav = document.getElementById("dropdown-nav");
+const toggleButtonClose = document.getElementById("toggle-btn-close")
 
 toggleButton.addEventListener("click",()=>{
     dropdownNav.style.display = "flex"
+    toggleButton.style.display ="none"
+    toggleButtonClose.style.display = "block"
+})
+
+toggleButtonClose.addEventListener("click",()=>{
+    dropdownNav.style.display = "none"
+    toggleButtonClose.style.display = "none"
+    toggleButton.style.display ="block"
 })
